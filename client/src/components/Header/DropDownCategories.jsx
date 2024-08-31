6a4e5fcd87b299ba6d4d68c3d3c8ed9a76fe3f34
Sufian <sufian0300@gmail.com>
2024-08-31 22:29:36 +0500

    Categories dropdown upgraded to multi level dropdown

--- client/src/components/Header/DropDownCategories.jsx
+++ client/src/components/Header/DropDownCategories.jsx
@@ -2,34 +2,140 @@ import React, { useState } from 'react';
 import '../../css/dropdowncategories.css';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faListUl } from '@fortawesome/free-solid-svg-icons';
+import { Dropdown, Space } from 'antd';
 
-const DropdownMenu = () => {
-    const [isDropdownVisible, setDropdownVisible] = useState(false);
+const DropDownCategories = () => {
+
+    const items = [
+        {
+            key: '1',
+            label: 'Art Supplies',
+            children: [
+                {
+                    key: '1-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '1-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '2',
+            label: 'Craft Supplies',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '3',
+            label: 'Office Supplies',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '4',
+            label: 'Organizers',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '5',
+            label: 'Paper Products',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '6',
+            label: 'Presentation Tools',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '7',
+            label: 'School Supplies',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+        {
+            key: '8',
+            label: 'Writing Tools',
+            children: [
+                {
+                    key: '2-1',
+                    label: '3rd menu item',
+                },
+                {
+                    key: '2-2',
+                    label: '4th menu item',
+                },
+            ],
+        },
+
+    ];
 
     return (
-        <div
-            className="dropdown-container"
-            onMouseEnter={() => setDropdownVisible(true)}
-            onMouseLeave={() => setDropdownVisible(false)}
-        >
-            <FontAwesomeIcon icon={faListUl} />
-            <button className="dropdown-button">Browse Categories</button>
-            {isDropdownVisible && (
-                <div className="dropdown-content">
-                    <ul>
-                        <li>Calculators</li>
-                        <li>Conference Pad</li>
-                        <li>Files & Folders</li>
-                        <li>Artist Supplies</li>
-                        <li>School Supplies</li>
-                        <li>Tools</li>
-                        <li>Photo Paper</li>
-                        <li>Notebooks</li>
-                    </ul>
-                </div>
-            )}
-        </div>
-    );
-};
 
-export default DropdownMenu;
+        <Dropdown
+            menu={{
+                items,
+            }}
+        >
+                <div className="dropdown-container">
+                     <FontAwesomeIcon icon={faListUl} />
+                    <button className="dropdown-button">Browse Categories</button>
+                    {/* <DownOutlined /> */}
+                </div>
+            
+        </Dropdown>
+    );
+}
+export default DropDownCategories;
